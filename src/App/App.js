81575cac3ae2969c8366bf162655a9d6557cfe39
Conner McCabe <conner.p.mccabe@gmail.com>
2020-09-21 10:41:45 -0500

    adding editor component

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import Editor from './Editor'
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+    </div>
+    <div className="pane">
+      <iframe 
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder='0'
+        width="100%"
+        height="100%"
+      />
+    </div>
     </div>
   );
 }
