54db309101133fd8f907536d86f851a8e596b457
Conner McCabe <conner.p.mccabe@gmail.com>
2020-09-21 12:17:50 -0500

    adding styling

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Editor from './Editor'
 
 function App() {
+  const [ html, setHtml ] = useState('')
+  const [ css, setCss ] = useState('')
+  const [ js, setJs] = useState('')
+
   return (
     <div className="App">
       <div className="pane top-pane">
-        <Editor />
-        <Editor />
-        <Editor />
+        <Editor 
+          language='xml'
+          displayName='HTML'
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor 
+          language='css'
+          displayName='CSS'
+          value={css}
+          onChange={setCss}
+        />
+        <Editor 
+          language='javascript'
+          displayName='JS'
+          value={js}
+          onChange={setJs}
+        />
     </div>
     <div className="pane">
       <iframe 
